682eb7018fdc1c540a7d8b52061f5a2a62526dab
zema <zlobzn@gmail.com>
2020-04-06 17:10:46 +0300

    added tester

---
+++ tester/tester-favicon.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd tmp
+
+while true; do
+    clear
+    date
+    echo ""
+    # wget -E -H -k -p http://127.0.0.1:3000/
+    # time curl -sSL -i -H "Accept-Encoding: gzip" -X GET http://127.0.0.1:3000/segseg
+    time curl -sSL -i -X GET http://127.0.0.1:3000/favicon.ico
+    sleep 1
+done
