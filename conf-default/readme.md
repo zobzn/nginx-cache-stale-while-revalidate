15b983ef254d322fac0fce6367d0f9b34af44f03
zema <zlobzn@gmail.com>
2020-04-06 17:10:19 +0300

    first commit

---
+++ conf-default/readme.md
@@ -0,0 +1,3 @@
+# Default nginx config files
+
+https://github.com/nginx/nginx/tree/master/conf
---
+++ www/index.php
@@ -0,0 +1,7 @@
+<?php // sleep(2)?>
+<?php usleep(1 * 1000 * 1000) ?>
+<!DOCTYPE html>
+<meta charset="UTF-8" />
+<link rel="stylesheet" type="text/css" href="/webpack/test-1.css" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<?= (new DateTimeImmutable())->setTimezone(new DateTimeZone('Europe/Kiev'))->format('Y-m-d H:i:s') . ' ' . $_SERVER['REQUEST_URI'] . PHP_EOL ?>
